5de39f76bbd76a374da26d0a71c169fe62533708
artemis <marlkiller@voidm.com>
2024-01-06 23:51:42 +0800

    Initial Commit

---
+++ InlineInjectPlugin/InlineInjectPlugin.h
@@ -0,0 +1,12 @@
+//
+//  InlineInjectPlugin.h
+//  InlineInjectPlugin
+//
+//  Created by 马治武 on 2024/1/6.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface InlineInjectPlugin : NSObject
+
+@end
---
+++ InlineInjectPlugin/InlineInjectPlugin.m
@@ -0,0 +1,12 @@
+//
+//  InlineInjectPlugin.m
+//  InlineInjectPlugin
+//
+//  Created by 马治武 on 2024/1/6.
+//
+
+#import "InlineInjectPlugin.h"
+
+@implementation InlineInjectPlugin
+
+@end
